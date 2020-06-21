65783651f70fe377df24c8cf3164d52323bdb696
Mikhail-Ark <mikhail.ark@yandex.ru>
2020-06-22 00:02:38 +0300

    match_dict to .py

--- libs/determinator.py
+++ libs/determinator.py
@@ -1,14 +1,14 @@
 from re import sub
 import pickle
 
+from info.match_dict import match_dict
 
-def determine_good_id(s, model=None):
+
+def determine_good_id(s):
     ns = weak_unification(s)
     if not ns:
         return None
-    if model is None:
-        model = get_model()
-    return model.get(ns, None)
+    return match_dict.get(ns, None)
 
 
 def weak_unification(s):
@@ -18,14 +18,7 @@ def weak_unification(s):
     return ns
 
 
-def get_model():
-    with open("info/match_dict", "rb") as f:
-        model = pickle.load(f)
-    return model
-
-
 def add_good_id_to_raws(raws):
-    model = get_model()
     for i, row in enumerate(raws):
-        raws[i] = row + (determine_good_id(row[0], model),)
+        raws[i] = row + (determine_good_id(row[0]),)
     return raws
